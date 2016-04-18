062ec42cd16e2d0aac24359919beee286335f5d6
François Néron <francois.n3ron@gmail.com>
2016-04-18 15:36:43 -0400

    Initial package commit.
    A package for paysafe api on meteorjs.

---
+++ paysafe-meteor.js
@@ -0,0 +1,5 @@
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See paysafe-meteor-tests.js for an example of importing.
+export const name = 'paysafe-meteor';
